731595527bcf14e6ba21843c4ce598b4a4a890e2
Novout <novout@hotmail.com>
2022-10-30 14:22:29 -0300

    feat(ui): total terrain producion in structure case

--- src/ui/terrain/info/GameTerrainInfoStructureItem.vue
+++ src/ui/terrain/info/GameTerrainInfoStructureItem.vue
@@ -5,12 +5,12 @@
       <IconLumber v-if="terrain.structure === 'lumber'" class="w-20 h-20" />
       <IconAcademicCenter v-if="terrain.structure === 'academic_center'" class="w-20 h-20" />
     </div>
-    <div class="flex flex-col gap-2">
-      <H2 v-if="define.resources">Produção</H2>
-      <Food v-if="define.resources?.food">{{ define.resources.food[0] }}</Food>
-      <Production v-if="define.resources?.production">{{ define.resources.production[0] }}</Production>
-      <Influence v-if="define.resources?.influence">{{ define.resources.influence[0] }}</Influence>
-      <Science v-if="define.resources?.science">{{ define.resources.science[0] }}</Science>
+    <div v-if="define.resources" class="flex flex-col gap-2">
+      <H2>Produção do Terreno</H2>
+      <Food>{{ terrain.resources.food + (define.resources?.food ? define.resources.food[0] : 0) }}</Food>
+      <Production>{{ terrain.resources.production + (define.resources?.production ? define.resources.food[0] : 0) }}</Production>
+      <Influence>{{ terrain.resources.influence + (define.resources?.influence ? define.resources.influence[0] : 0) }}</Influence>
+      <Science>{{ terrain.resources.science + (define.resources?.science ? define.resources.science[0] : 0) }}</Science>
       <H2 v-if="define.multipliers">Multiplicador</H2>
       <Food v-if="define.multipliers?.food">{{ String(define.multipliers.food[0]).substring(2) }}0%</Food>
       <Production v-if="define.multipliers?.production">{{ String(define.multipliers.production[0]).substring(2) }}0%</Production>
